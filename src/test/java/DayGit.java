360c3c3fe4030aeb4bd2acbb3972684146777a05
ThuyNT2204 <nguyenthithuy22042003@gmail.com>
2026-03-06 16:02:05 +0700

    ontap

--- src/test/java/DayGit.java
+++ src/test/java/DayGit.java
@@ -1,10 +1,14 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DayGit {
     WebDriver driver;
+    String url = "http://vitimex.com.vn/search";
     @BeforeEach
     public void setup() {
         ChromeOptions options = new ChromeOptions();
@@ -13,5 +17,20 @@ public class DayGit {
         options.addArguments("--disable-dev-shm-usage");
 
         driver = new ChromeDriver(options);
+        //driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+    @Test
+    public void timKiem() throws InterruptedException {
+        driver.get(url);
+        driver.findElement(By.className("search_box")).sendKeys("Ao");
+        driver.findElement(By.cssSelector("input[type='submit'")).click();
+        Thread.sleep(5000);
+        System.out.println("Tim kiem thanh cong");
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
 }
